1adc8fa285ce1488a8af7cb0bdc0cf56dd570145
GabrielPyrrho <pyrrhogabriel17@gmail.com>
2024-05-16 18:01:47 -0300

    Segundo Commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Calcular Programa
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+function clearDisplay(){
+    display.value = "";
+}
+
+function Calcular(){
+        display.value = eval(display.value);
+    }
+
